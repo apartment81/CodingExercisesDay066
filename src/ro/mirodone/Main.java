6aa005c1fbdfb9e1ada8a4e2462cdc9f0addec63
apartment81 <bogdan.mirodone@gmail.com>
2019-08-21 23:55:14 +0300

    roman numbers

---
+++ src/ro/mirodone/Main.java
@@ -0,0 +1,12 @@
+package ro.mirodone;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        RomanNumerals romanNumerals = new RomanNumerals();
+        System.out.println(romanNumerals.solution(50));
+        System.out.println(romanNumerals.solution(2019));
+
+    }
+}
